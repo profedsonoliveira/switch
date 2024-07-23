ce729cecb16ea280698e0841c98ebc7bf8d9dc19
profedsonoliveira <prof.edson.oliveira@hotmail.com>
2024-07-23 12:39:11 -0300

    switch

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int a, b;
+
+Console.WriteLine("Digite dois valores positivos: ");
+a = Convert.ToInt32(Console.ReadLine());    
+b = Convert.ToInt32(Console.ReadLine());
+
+
+switch ((a, b))
+{
+    case  (> 0, > 0) when a == b:
+        Console.WriteLine($"Os valores são válidos e iguais {a}.");
+        break;
+
+    case ( > 0, > 0):
+        Console.WriteLine($"valores válidos distindos a = {a} , b =  {b}.");
+        break;
+
+    default:
+        Console.WriteLine("Um ou os dois valores não são positivos");
+        break;
+}
+
+
